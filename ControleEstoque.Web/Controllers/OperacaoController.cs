1b7ff20f1eba033072f1b4ae17d42c3f56ac38a4
AnsuGomes <gomesdasilva1996@gmail.com>
2022-07-21 19:28:54 -0300

    Add project files.

---
+++ ControleEstoque.Web/Controllers/OperacaoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControleEstoque.Web.Controllers
+{
+    public class OperacaoController : Controller
+    {
+        // GET: Operacao
+        public ActionResult EntradaEstoque()
+        {
+            return View();
+        }
+
+        public ActionResult SaídaEstoque()
+        {
+            return View();
+        }
+
+        public ActionResult LancPerdaProduto()
+        {
+            return View();
+        }
+
+        public ActionResult inventario()
+        {
+            return View();
+        }
+    }
+}
