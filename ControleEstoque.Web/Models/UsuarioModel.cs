f7975fbd45b030930343651e658a2235f4539d44
Anderson Gomes <100328879+AnsuGomes@users.noreply.github.com>
2022-10-21 22:50:41 -0300

    Cegurança da aplicação #1 - Criptografando a senha.

--- ControleEstoque.Web/Models/UsuarioModel.cs
+++ ControleEstoque.Web/Models/UsuarioModel.cs
@@ -5,6 +5,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
+
 namespace ControleEstoque.Web.Models
 {
     public class UsuarioModel
@@ -19,8 +20,9 @@ namespace ControleEstoque.Web.Models
                 using (var comando = new SqlCommand())
                 {
                     comando.Connection = conexao;
-                    comando.CommandText = string.Format(
-                        "select count (*) from usuario where login = '{0}' and senha = '{1}'", login, senha);
+                    comando.CommandText = string.Format(                                               
+                        "select count(*) from usuario where login='{0}' and senha='{1}'",
+                        login, CriptoHelper.HashMD5(senha));
                     ret = ((int)comando.ExecuteScalar() > 0);
                 }
             }
