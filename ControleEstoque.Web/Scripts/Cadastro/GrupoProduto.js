5a5334a2b208e4f580eea7453934268357744d7d
Anderson Gomes <100328879+AnsuGomes@users.noreply.github.com>
2023-03-29 19:06:53 -0300

    Standardization of the register. #20

--- ControleEstoque.Web/Scripts/Cadastro/GrupoProduto.js
+++ ControleEstoque.Web/Scripts/Cadastro/GrupoProduto.js
@@ -2,20 +2,26 @@
     data.__RequestVerificationToken = $('[name=__RequestVerificationToken]').val();
     return data;
 }
+
+function formatar_mensagem_aviso(mensagens) {
+    var ret = '';
+    for (var i = 0; i < mensagens.length; i++) {
+        ret += '<li>' + mensagens[i] + '</li>';
+    }
+    return '<ul>' + ret + '</ul>';
+}
+
 function abrir_form(dados) {
     $('#id_cadastro').val(dados.Id);
     $('#txt_nome').val(dados.Nome);
     $('#cbx_ativo').prop('checked', dados.Ativo);
-
     var modal_cadastro = $('#modal_cadastro');
-
     $('#msg_mensagem_aviso').empty();
     $('#msg_aviso').hide();
     $('#msg_mensagem_aviso').hide();
     $('#msg_erro').hide();
-
     bootbox.dialog({
-        title: '@ViewBag.Title',
+        title: tituloPagina,
         message: modal_cadastro
     })
         .on('shown.bs.modal', function () {
@@ -27,7 +33,6 @@ function abrir_form(dados) {
             modal_cadastro.hide().appendTo('body');
         });
 }
-
 function criar_linha_grid(dados) {
     var ret =
         '<tr data-id=' + dados.Id + '>' +
@@ -40,14 +45,13 @@ function criar_linha_grid(dados) {
         '</tr>';
     return ret;
 }
-
 $(document).on('click', '#btn_incluir', function () {
     abrir_form({ Id: 0, Nome: '', Ativo: true });
 })
     .on('click', '.btn-alterar', function () {
         var btn = $(this),
             id = btn.closest('tr').attr('data-id'),
-            url = '@Url.Action("RecuperarGrupoProduto", "Cadastro")',
+            url = url_alterar,
             param = { 'id': id };
         $.post(url, add_anti_forgery_token(param), function (response) {
             if (response) {
@@ -55,23 +59,22 @@ $(document).on('click', '#btn_incluir', function () {
             }
         });
     })
-
     .on('click', '.btn-excluir', function () {
         var btn = $(this),
             tr = btn.closest('tr'),
             id = tr.attr('data-id'),
-            url = '@Url.Action("ExcluirGrupoProduto", "Cadastro")',
+            url = url_Excluir,
             param = { 'id': id };
         bootbox.confirm({
             message: "Realmente deseja excluir o grupo de produto?",
             buttons: {
                 confirm: {
                     label: 'Sim',
-                    className: 'btn-success'
+                    className: 'btn-danger'
                 },
                 cancel: {
                     label: 'Não',
-                    className: 'btn-danger'
+                    className: 'btn-success'
                 }
             },
             callback: function (result) {
@@ -87,19 +90,18 @@ $(document).on('click', '#btn_incluir', function () {
     })
     .on('click', '#btn_confirmar', function () {
         var btn = $(this),
-            url = '@Url.Action("SalvarGrupoProduto", "Cadastro")',
+            url = url_Confirmar,
             param = {
                 Id: $('#id_cadastro').val(),
                 Nome: $('#txt_nome').val(),
                 Ativo: $('#cbx_ativo').prop('checked')
             };
         $.post(url, add_anti_forgery_token(param), function (response) {
-            if (response.Resultado == "Ok") {
+            if (response.Resultado == "OK") {
                 if (param.Id == 0) {
                     param.Id = response.IdSalvo;
                     var table = $('#grid_cadastro').find('tbody'),
                         linha = criar_linha_grid(param);
-
                     table.append(linha);
                 }
                 else {
@@ -108,29 +110,55 @@ $(document).on('click', '#btn_incluir', function () {
                         .eq(0).html(param.Nome).end()
                         .eq(1).html(param.Ativo ? 'SIM' : 'NÃO');
                 }
-
                 $('#modal_cadastro').parents('.bootbox').modal('hide');
             }
-            else if (response.Resultado == "ERRO!") {
+            else if (response.Resultado == "ERRO") {
                 $('#msg_aviso').hide();
                 $('#msg_mensagem_aviso').hide();
                 $('#msg_erro').show();
             }
-            else if (response.Resultado == "AVISO!") {
+            else if (response.Resultado == "AVISO") {
                 $('#msg_mensagem_aviso').html(formatar_mensagem_aviso(response.Mensagens));
                 $('#msg_aviso').show();
                 $('#msg_mensagem_aviso').show();
                 $('#msg_erro').hide();
             }
         });
+    })
+    .on('click', '.page-item', function () {
+        var btn = $(this),
+            tamPag = $('#ddl_tam_pag').val(),
+            pagina = btn.text(),
+            url = url_page_click,
+            param = { 'pagina': pagina, 'tamPag': tamPag };
+        $.post(url, add_anti_forgery_token(param), function (response) {
+            if (response) {
+                var table = $('#grid_cadastro').find('tbody');
+                table.empty();
+                for (var i = 0; i < response.length; i++) {
+                    table.append(criar_linha_grid(response[i]));
+                }
+                btn.siblings().removeClass('active');
+                btn.addClass('active');
+            }
+        });
+    })
+    .on('change', '#ddl_tam_pag', function () {
+        var ddl = $(this),
+            tamPag = ddl.val(),
+            pagina = 1,
+            url = url_tam_pag_change,
+            param = { 'pagina': pagina, 'tamPag': tamPag };
+        $.post(url, add_anti_forgery_token(param), function (response) {
+            if (response) {
+                var table = $('#grid_cadastro').find('tbody');
+                table.empty();
+                for (var i = 0; i < response.length; i++) {
+                    table.append(criar_linha_grid(response[i]));
+                }
+                btn.siblings().removeClass('active');
+                btn.addClass('active');
+            }
+        });
     });
 
-function formatar_mensagem_aviso(mensagens) {
-    var ret = "";
-
-    for (var i = 0; i < mensagens.length; i++) {
-        ret += '<li>' + mensagens[i] + '</li>';
-    }
-
-    return '<ul>' + ret + '</ul>';
-}
