a5beed286dfaf229cd69f5a9aae8d30bbc6e32f0
Anderson Gomes <100328879+AnsuGomes@users.noreply.github.com>
2023-04-07 11:34:00 -0300

    Registration of measurement units. #22

--- ControleEstoque.Web/Models/UnidadeMedidaModel.cs
+++ ControleEstoque.Web/Models/UnidadeMedidaModel.cs
@@ -3,13 +3,18 @@ using System.ComponentModel.DataAnnotations;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+
 namespace ControleEstoque.Web.Models
 {
-    public class GrupoProdutoModel
+    public class UnidadeMedidaModel
     {
         public int Id { get; set; }
+
         [Required(ErrorMessage = "Preencha o nome.")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Preencha a sigla.")]
+        public string Sigla { get; set; }
 
         public bool Ativo { get; set; }
 
@@ -24,15 +29,17 @@ namespace ControleEstoque.Web.Models
                 using (var comando = new SqlCommand())
                 {
                     comando.Connection = conexao;
-                    comando.CommandText = "select count(*) from grupo_produto";
+                    comando.CommandText = "select count(*) from unidade_medida";
                     ret = (int)comando.ExecuteScalar();
                 }
             }
+
             return ret;
         }
-        public static List<GrupoProdutoModel> RecuperarLista(int pagina, int tamPagina)
+
+        public static List<UnidadeMedidaModel> RecuperarLista(int pagina, int tamPagina)
         {
-            var ret = new List<GrupoProdutoModel>();
+            var ret = new List<UnidadeMedidaModel>();
 
             using (var conexao = new SqlConnection())
             {
@@ -44,25 +51,29 @@ namespace ControleEstoque.Web.Models
 
                     comando.Connection = conexao;
                     comando.CommandText = string.Format(
-                        "select * from grupo_produto order by nome offset {0} rows fetch next {1} rows only",
+                        "select * from unidade_medida order by nome offset {0} rows fetch next {1} rows only",
                         pos > 0 ? pos - 1 : 0, tamPagina);
                     var reader = comando.ExecuteReader();
                     while (reader.Read())
                     {
-                        ret.Add(new GrupoProdutoModel
+                        ret.Add(new UnidadeMedidaModel
                         {
                             Id = (int)reader["id"],
                             Nome = (string)reader["nome"],
+                            Sigla = (string)reader["sigla"],
                             Ativo = (bool)reader["ativo"]
                         });
                     }
                 }
             }
+
             return ret;
         }
-        public static GrupoProdutoModel RecuperarPeloId(int id)
+
+        public static UnidadeMedidaModel RecuperarPeloId(int id)
         {
-            GrupoProdutoModel ret = null;
+            UnidadeMedidaModel ret = null;
+
             using (var conexao = new SqlConnection())
             {
                 conexao.ConnectionString = ConfigurationManager.ConnectionStrings["principal"].ConnectionString;
@@ -70,25 +81,31 @@ namespace ControleEstoque.Web.Models
                 using (var comando = new SqlCommand())
                 {
                     comando.Connection = conexao;
-                    comando.CommandText = "select * from grupo_produto where (id = @id)";
+                    comando.CommandText = "select * from unidade_medida where (id = @id)";
+
                     comando.Parameters.Add("@id", SqlDbType.Int).Value = id;
+
                     var reader = comando.ExecuteReader();
                     if (reader.Read())
                     {
-                        ret = new GrupoProdutoModel
+                        ret = new UnidadeMedidaModel
                         {
                             Id = (int)reader["id"],
                             Nome = (string)reader["nome"],
+                            Sigla = (string)reader["sigla"],
                             Ativo = (bool)reader["ativo"]
                         };
                     }
                 }
             }
+
             return ret;
         }
+
         public static bool ExcluirPeloId(int id)
         {
             var ret = false;
+
             if (RecuperarPeloId(id) != null)
             {
                 using (var conexao = new SqlConnection())
@@ -98,18 +115,24 @@ namespace ControleEstoque.Web.Models
                     using (var comando = new SqlCommand())
                     {
                         comando.Connection = conexao;
-                        comando.CommandText = "delete from grupo_produto where (id = @id)";
+                        comando.CommandText = "delete from unidade_medida where (id = @id)";
+
                         comando.Parameters.Add("@id", SqlDbType.Int).Value = id;
+
                         ret = (comando.ExecuteNonQuery() > 0);
                     }
                 }
             }
+
             return ret;
         }
+
         public int Salvar()
         {
             var ret = 0;
+
             var model = RecuperarPeloId(this.Id);
+
             using (var conexao = new SqlConnection())
             {
                 conexao.ConnectionString = ConfigurationManager.ConnectionStrings["principal"].ConnectionString;
@@ -117,19 +140,26 @@ namespace ControleEstoque.Web.Models
                 using (var comando = new SqlCommand())
                 {
                     comando.Connection = conexao;
+
                     if (model == null)
                     {
-                        comando.CommandText = "insert into grupo_produto (nome, ativo) values (@nome, @ativo); select convert(int, scope_identity())";
+                        comando.CommandText = "insert into unidade_medida (nome, sigla, ativo) values (@nome, @sigla, @ativo); select convert(int, scope_identity())";
+
                         comando.Parameters.Add("@nome", SqlDbType.VarChar).Value = this.Nome;
+                        comando.Parameters.Add("@sigla", SqlDbType.VarChar).Value = this.Sigla;
                         comando.Parameters.Add("@ativo", SqlDbType.VarChar).Value = (this.Ativo ? 1 : 0);
+
                         ret = (int)comando.ExecuteScalar();
                     }
                     else
                     {
-                        comando.CommandText = "update grupo_produto set nome=@nome, ativo=@ativo where id = @id";
+                        comando.CommandText = "update unidade_medida set nome=@nome, sigla=@sigla, ativo=@ativo where id = @id";
+
                         comando.Parameters.Add("@nome", SqlDbType.VarChar).Value = this.Nome;
+                        comando.Parameters.Add("@sigla", SqlDbType.VarChar).Value = this.Sigla;
                         comando.Parameters.Add("@ativo", SqlDbType.VarChar).Value = (this.Ativo ? 1 : 0);
                         comando.Parameters.Add("@id", SqlDbType.Int).Value = this.Id;
+
                         if (comando.ExecuteNonQuery() > 0)
                         {
                             ret = this.Id;
@@ -137,6 +167,7 @@ namespace ControleEstoque.Web.Models
                     }
                 }
             }
+
             return ret;
         }
     }
